0b6b42e364a7b72328b542abb71f454a10200a02
kf456 <kf893136@gmail.com>
2026-02-23 15:35:51 +0500

    practise successful

---
+++ classs.py
@@ -0,0 +1,23 @@
+#comments
+#this code is replacing empty space in the text
+#variable understanding
+#user-age = 22
+#User-age = 44
+#user-age = 33
+#a = 12
+# age = 20
+# name  "zeehan"
+
+##print(user-age,User-age)
+##data types
+#numeric
+# integer: 23424, -3.0 -1.0
+#float:234.098, -23.0, -1.0 
+#text
+#string: :"we ae learning base", "abc garden town", "text
+# #boolean: true,false
+# ##collections
+#list: cloud-class-python = ["ali","hamza","abc"]
+#tuple cloud-class = {"ali", "hamza", "abc", "aaa"}
+#dictionary:student-data = {"name":"ali", "age":17"adress":"abc street multan"}
+#set: {2,14,12}
