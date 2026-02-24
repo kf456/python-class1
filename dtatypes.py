3ed4124c8dffc5e7a1f6b3581a3a445afc1c8dac
kf456 <kf893136@gmail.com>
2026-02-24 13:29:55 +0500

    python

---
+++ dtatypes.py
@@ -0,0 +1,35 @@
+price = 450
+salary = 4500.45
+name = "ali"
+item = ["apple", "book","laptop", "chair"]
+
+
+static = ("u009" ,"1234","89")
+
+values = {12,34,67,78}
+print(type(price))
+print(type(salary))
+print(type(name))
+print(type(item))
+print(type(static))
+print(type(values))
+
+user_imfo = { "name": 'ali', "age": '23', "address": 'buch villas', "city": 'multan', "country": 'pakistan'}
+print(type(user_imfo))
+value1 = 23
+value2 = 45
+sum1 = value1 + value2
+print("adding sum one valueby using function sum",(23,45))
+
+
+value4 = 89
+value5 = 67
+sum1 = value4 + value5
+print("adding sum one valueby using function sum",(89,67))
+
+
+
+print("we are learning asdfasfasf",value4)
+
+      
+
